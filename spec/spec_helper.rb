dad1baf36164c8af8df8bbd6ceb00ee65df26efd
Evan David Light <evan@tiggerpalace.com>
2008-05-29 09:14:11 -0700

    Um... woops....  Maybe my specs should use Mocha?  Yeah....

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,8 @@ require File.dirname(__FILE__) + "/../lib/jrsplenda.rb"
 
 require File.dirname(__FILE__) + "/../build/jrsplenda-fixtures.jar"
 
+Spec::Runner.configure do |config|
+  config.mock_with :mocha
+end
+
 include Java
