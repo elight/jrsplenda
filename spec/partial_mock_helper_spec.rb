047c6f8e3b9a440642e89882fc742e5e59421ca2
Evan David Light <evan@tiggerpalace.com>
2008-07-28 01:13:17 -0400

    Added partial mock spec

---
+++ spec/partial_mock_helper_spec.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe "A mock helper" do
+  include JRSplenda::MockHelper
+  
+  it "should create a partial mock of a Java class when give a Java class name when the Java class has not been imported" do
+    splenda_partial_mock('fixtures.PrivateField').should be_a_kind_of(PrivateField)
+  end
+
+  it "should create a partial mock of a Java class when give a Java class name when the Java class has been imported" do
+    splenda_partial_mock('fixtures.PrivateField').should be_a_kind_of(PrivateField)
+  end
+  
+  it "should create a partial mock of a Java class when given a Ruby class wrapping the Java class" do
+    import 'fixtures.PrivateField'
+    splenda_partial_mock(PrivateField).should be_a_kind_of(PrivateField)
+  end
+
+  it "should not blankslate the partially mocked object" do
+    mock = splenda_partial_mock('fixtures.PrivateInstanceMethod')
+    mock.should respond_to(:thePrivateMethod)
+    mock.thePrivateMethod.should == "42"
+  end
+
+  it "should replace a method with an expectation when the #expects method is used" do
+    mock = splenda_partial_mock('fixtures.PrivateInstanceMethod')
+    mock.expects(:thePrivateMethod).and_returns("43")
+    mock.thePrivateMethod.should == "43"
+  end
+    
+  describe "when creating a partial mock object" do
+    it "should optionally not overwrite existing member variables" do
+      splenda_partial_mock_attr('fixtures.PrivateField')
+      p1 = @private_field
+      splenda_partial_mock_attr('fixtures.PrivateField', :preserve_existing_attr => true)
+      p1.should equal(@private_field)
+    end
+  end
+end
