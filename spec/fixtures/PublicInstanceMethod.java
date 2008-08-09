6bbe8615e0478b13e47cada34cd016d75be5b27d
Evan David Light <evan@tiggerpalace.com>
2008-08-09 11:33:09 -0400

    Added missing fixture

---
+++ spec/fixtures/PublicInstanceMethod.java
@@ -0,0 +1,12 @@
+package fixtures;
+
+public class PublicInstanceMethod 
+{
+  public String thePublicMethod1() {
+    return "42";
+  }
+  
+  public String thePublicMethod2() {
+    return "1764";
+  }
+}
